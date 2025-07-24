ffde6491b7ab642bccde715b0833d2c52431e0ca
gokalpyildiz <gklpyldz2050@gmail.com>
2025-07-24 08:25:56 +0300

    notification improved with local notification

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -42,7 +43,9 @@ android {
         }
     }
 }
-
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
 flutter {
     source = "../.."
 }
--- lib/product/firebase/firebase_core/firebase_messaging.dart
+++ lib/product/firebase/firebase_core/firebase_messaging.dart
@@ -4,6 +4,7 @@ import 'package:etkinlik_takip/product/utility/project_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FbMessaging {
   static String? fcmToken;
@@ -11,12 +12,20 @@ class FbMessaging {
   static final FbMessaging _instance = FbMessaging._init();
   static FbMessaging get instance => _instance;
   final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+  static const CHANNEL_ID = 'default';
+  static const CHANNEL_NAME = 'default';
+  final FlutterLocalNotificationsPlugin _localNotifications = FlutterLocalNotificationsPlugin();
 
   Future<void> init() async {
     _fcm.requestPermission();
+    const AndroidInitializationSettings androidSettings = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initSettings = InitializationSettings(android: androidSettings);
+    await _localNotifications.initialize(initSettings);
     //todo push notification modülü eklenince açılacak
     if (!Platform.isIOS) {
       fcmToken = await _fcm.getToken();
+    } else {
+      fcmToken = await _fcm.getAPNSToken();
     }
     _fcm.onTokenRefresh.listen((newToken) async {
       fcmToken = newToken;
@@ -43,17 +52,27 @@ class FbMessaging {
   void _handleMessage1(RemoteMessage? message) {
     debugPrint('onMessageOpenedApp');
     ProjectManager.scaffoldMessengerKey.currentState?.showSnackBar(
-      SnackBar(content: Text((message?.data.toString() ?? 'null') + 'onMessageOpenedApp')),
+      SnackBar(content: Text('${message?.data.toString() ?? 'null'}onMessageOpenedApp')),
     );
   }
 
-  void _handleMessage2(RemoteMessage? message) {
+  Future<void> _handleMessage2(RemoteMessage? message) async {
     debugPrint('onMessage');
-    ProjectManager.scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(content: Text((message?.data.toString() ?? 'null') + 'onMessage')));
+    ProjectManager.scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(content: Text('${message?.data.toString() ?? 'null'}onMessage')));
+    const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      CHANNEL_ID,
+      CHANNEL_NAME,
+      channelDescription: 'Etkinlik Takibi',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails details = NotificationDetails(android: androidDetails);
+    var notification = message?.notification;
+    await _localNotifications.show(notification.hashCode, notification?.title, notification?.body, details);
   }
 
   void _handleMessage3(RemoteMessage? message) {
     debugPrint('initialmessage');
-    ProjectManager.scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(content: Text((message?.data.toString() ?? 'null') + 'initialmessage')));
+    ProjectManager.scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(content: Text('${message?.data.toString() ?? 'null'}initialmessage')));
   }
 }
