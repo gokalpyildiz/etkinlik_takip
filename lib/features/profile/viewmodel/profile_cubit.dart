a0b0b6a851e7acd780f26bde301300500ae13726
gokalpyildiz <gklpyldz2050@gmail.com>
2025-07-25 09:15:16 +0300

    fixed token clear on signout

--- lib/features/profile/viewmodel/profile_cubit.dart
+++ lib/features/profile/viewmodel/profile_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:etkinlik_takip/data/models/auth_models/user_model.dart';
+import 'package:etkinlik_takip/data/services/auth_service/IAuthService.dart';
+import 'package:etkinlik_takip/data/services/auth_service/auth_service.dart';
+import 'package:etkinlik_takip/product/functions/auth_operation.dart';
 import 'package:etkinlik_takip/product/state/base/base_cubit.dart';
 import 'package:etkinlik_takip/product/utility/firebase/firebase_collections.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,9 +11,12 @@ import 'package:flutter/material.dart';
 part 'profile_state.dart';
 
 class ProfileCubit extends BaseCubit<ProfileState> {
-  ProfileCubit() : super(ProfileState());
+  ProfileCubit() : super(ProfileState()) {
+    _service = AuthService.init();
+  }
   User? user;
   UserModel? userModel;
+  late IAuthService _service;
   Future<void> refreshBottomVies() async {
     emit(state.copyWith(isLoading: true));
     try {
@@ -23,5 +29,15 @@ class ProfileCubit extends BaseCubit<ProfileState> {
       debugPrint(e.toString());
     }
     emit(state.copyWith(isLoading: false));
+  }
+
+  Future<bool> signOut() async {
+    var response = await _service.signOut();
+    if (response?.success ?? false) {
+      AuthOperation.instance.clear();
+      return true;
+    } else {
+      return false;
+    }
   }
 }
